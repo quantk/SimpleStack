800348fca38ef3de72061367e9a8ba45b97ca7da
Alexander Kladov <mail@kladov.xyz>
2017-01-15 23:08:50 +0300

    test data provider

--- tests/SimpleStackTest.php
+++ tests/SimpleStackTest.php
@@ -16,18 +16,31 @@ class SimpleStackTest extends \PHPUnit_Framework_TestCase
     {
         $this->simpleStack = null;
     }
-
-    public function testStack() {
-        $data = 10;
+    /**
+     * @dataProvider stackDataProvider
+     */
+    public function testStack($data) {
         $this->simpleStack->push($data);
         $result = $this->simpleStack->pop();
         $this->assertEquals($data, $result);
     }
 
-    public function testStackPeek() {
+    /**
+     * @dataProvider stackDataProvider
+     */
+    public function testStackPeek($data) {
         $data = 10;
         $this->simpleStack->push($data);
         $result = $this->simpleStack->peek();
         $this->assertEquals($data, $result);
+    }
+
+    public function stackDataProvider() {
+        return [
+            [10],
+            [20],
+            [30],
+            ["Hello, Stack"],
+        ];
     }
 }
